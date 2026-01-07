6d71d6c599b29d9537bceee42d5138c77fffa974
FelekDevYT <felsstudio@mail.ru>
2026-01-07 15:41:26 +0300

    xd

--- src/main/java/me/alpha432/oyvey/manager/ModuleManager.java
+++ src/main/java/me/alpha432/oyvey/manager/ModuleManager.java
@@ -11,7 +11,6 @@ import me.alpha432.oyvey.features.modules.client.HudEditor;
 import me.alpha432.oyvey.features.modules.client.Notifications;
 import me.alpha432.oyvey.features.modules.combat.*;
 import me.alpha432.oyvey.features.modules.hud.Coordinates;
-import me.alpha432.oyvey.features.modules.hud.Fps;
 import me.alpha432.oyvey.features.modules.hud.Watermark;
 import me.alpha432.oyvey.features.modules.misc.*;
 import me.alpha432.oyvey.features.modules.movement.*;
@@ -29,7 +28,6 @@ public class ModuleManager implements Jsonable, Util {
 
     public void init() {
         register(new Watermark());
-        register(new Fps());
         register(new Coordinates());
         register(new HudEditor());
         register(new ClickGui());
